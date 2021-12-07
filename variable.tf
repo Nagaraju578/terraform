d4647deb0ff364939a55c8a91d62b05d24c35f7b
Nagaraju578 <80188560+Nagaraju578@users.noreply.github.com>
2021-12-07 17:53:20 +0530

    Update variable.tf
    Updated file

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Nagaraju9182.pem" 
+    default     =  "Nagaraju9182" 
 }
 
 variable "instance_type" { 
